01fe268906fb3d42a9ff234dbe965c695017a59d
Vladimir Klimontovich <klimontovich@gmail.com>
2011-05-13 21:17:48 +0400

    First version of java expression evaluator + tests

---
+++ src/main/java/jexpr/CompiledExpression.java
@@ -0,0 +1,11 @@
+package jexpr;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vklimontovich
+ * Date: 5/13/11
+ * Time: 7:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CompiledExpression {
+}
---
+++ src/test/java/jexpr/impl/JavaExpressionEvaluatorTest.java
@@ -0,0 +1,11 @@
+package jexpr.impl;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vklimontovich
+ * Date: 5/13/11
+ * Time: 8:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JavaExpressionEvaluatorTest {
+}
